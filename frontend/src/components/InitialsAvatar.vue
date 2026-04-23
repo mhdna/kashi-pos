af62f82f3dd7b3f3065f9c445646a65d5bf67e60
Mahdi <mhdna@outlook.com>
2026-04-23 15:06:04 +0300

    feat: add initialsAvatar component

---
+++ frontend/src/components/InitialsAvatar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-avatar :color="props.color" size="60">
+        <span style="letter-spacing: 1px; font-size: 25px;">{{ firstName[0] }}{{ lastName[0]
+        }}</span>
+    </v-avatar>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    color: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+})
+</script>
