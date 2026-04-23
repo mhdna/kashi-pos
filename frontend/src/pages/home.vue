11dc8c0a75b5a1b40fab5b178e3290b399e040bb
Mahdi <mhdna@outlook.com>
2026-04-23 20:40:53 +0300

    pages: move index to home and update its content to use DefaultLayout

---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex">
+    <div style="flex: 0 0 50%;">
+      <v-sheet>
+        <div>
+          <v-card-title>
+            Invoice
+          </v-card-title>
+          <v-card-subtitle>
+            SA/CN/2026/1441
+          </v-card-subtitle>
+        </div>
+        <v-text-field class="mx-4 mt-4" variant="solo-filled" hide-details="auto" label="Barcode"></v-text-field>
+        <InvoicesTable />
+      </v-sheet>
+    </div>
+    <div style="flex: 0 0 30%;">
+      <Calculator />
+    </div>
+    <div style="flex: 0 0 20%;">
+      <BottomBar />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import InvoicesTable from '@/components/InvoicesTable.vue';
+import BottomBar from '@/components/BottomBar.vue';
+import Calculator from '@/components/Calculator.vue';
+</script>
