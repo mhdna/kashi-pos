b2bdc04fb810f25ba7202d9a28e6dc39d9398937
Mahdi <mhdna@outlook.com>
2026-04-23 20:39:02 +0300

    navdrawer: add close shift card

---
+++ frontend/src/components/CloseShiftCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card min-height="600" max-height="600" title="Shift Summary" class="d-flex flex-column">
+        <v-card-text class="flex-grow-1">
+            <!-- your content here -->
+        </v-card-text>
+
+        <v-card-actions class="pa-2 ga-2">
+            <v-btn variant="elevated" class="flex-1-1">
+                Cancel
+            </v-btn>
+            <v-btn variant="elevated" color="red-darken-4" class="flex-1-1">
+                Close Shift
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
