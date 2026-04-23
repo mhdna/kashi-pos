19efc571eb314c825e518fe407d5e5dfdf2f9ccb
Mahdi <mhdna@outlook.com>
2026-04-23 20:39:38 +0300

    layouts: add default layous & initial routes

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <AppBar />
+        <NavDrawer />
+        <v-main>
+            <v-container fluid class="pa-0" style="height: 91vh; overflow: hidden;">
+                <router-view />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script setup>
+import AppBar from '@/components/AppBar.vue'
+import NavDrawer from '@/components/NavDrawer.vue'
+</script>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,17 +3,29 @@
  *
  * Manual routes for ./src/pages/*.vue
  */
-
-// Composables
 import { createRouter, createWebHistory } from 'vue-router'
-import Index from '@/pages/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/auth',
+      component: () => import('@/layouts/AuthLayout.vue'),
+      children: [
+        { path: '/login', component: () => import('@/pages/login.vue') },
+      ],
+    },
+    {
       path: '/',
-      component: Index,
+      component: () => import('@/layouts/DefaultLayout.vue'),
+      children: [
+        { path: 'home', component: () => import('@/pages/home.vue') },
+        { path: 'inventory', component: () => import('@/pages/inventory.vue') },
+        { path: 'transfers', component: () => import('@/pages/transfers.vue') },
+        { path: 'clients', component: () => import('@/pages/clients.vue') },
+        { path: 'attendance', component: () => import('@/pages/attendance.vue') },
+        { path: 'shifts', component: () => import('@/pages/shifts.vue') },
+      ],
     },
   ],
 })
