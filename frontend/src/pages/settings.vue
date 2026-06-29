ada8a3f51e729bb31a1f5a4334772ceef74c633f
Mahdi <mhdna@outlook.com>
2026-06-29 08:00:11 +0300

    add initial settings page

---
+++ frontend/src/pages/settings.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex" style="width: 100%; overflow: hidden">
+    <v-card
+      title="Printing Preferences"
+      class="ma-4 px-4"
+      min-height="400"
+      min-width="300"
+    >
+      <div>Device Name:</div>
+      <div>Device Name:</div>
+    </v-card>
+    <v-card title="API keys" class="ma-4 px-4" min-height="400" min-width="300">
+      <div>Device Name:</div>
+      <div>Device Name:</div>
+    </v-card>
+    <v-card
+      title="Branch Details"
+      class="ma-4 px-4"
+      min-height="400"
+      min-width="300"
+    >
+      <div>Name:</div>
+      <div>Attendance Machine IP Address:</div>
+    </v-card>
+  </div>
+</template>
